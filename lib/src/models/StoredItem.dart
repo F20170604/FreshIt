72b95f8de80528a14d7b2a1660e5163ff58842dd
Ayush Agrawal <f20170604@goa.bits-pilani.ac.in>
2019-04-09 01:22:02 +0530

    no message

--- lib/src/models/StoredItem.dart
+++
@@ -1,3 +0,0 @@
-import 'package:equatable/equatable.dart';
-
-class StoredItem extends Equatable {}
--- lib/src/models/User.dart
+++
@@ -1,7 +0,0 @@
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
-
-class User extends Equatable {
-  final String email;
-  User(this.email) : super([email]);
-}
